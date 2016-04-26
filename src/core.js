48e011d58e2518a0dfdc4f0d7a8a31fa80ad8141
Jenna Vuong <jennavuong@gmail.com>
2016-04-25 23:04:26 -0700

    add functions to create tally for ea vote, increment tally for ea vote

--- src/core.js
+++ src/core.js
@@ -1,4 +1,4 @@
-/* application allows 'loading in' a collection of entries that will be voted on */
+/* app allows 'loading in' a collection of entries that will be voted on */
 
 import { List } from 'immutable';
 
@@ -8,10 +8,20 @@ export function setEntries(state, entries) {
 }
 
 // start the voting by calling state that already has entries set
+// create a vote Map({ pair: list })
 export function next(state) {
 	const entries = state.get('entries');
 	return state.merge({
 		vote: Map({ pair: entries.take(2) }),
 		entries: entries.skip(2)
 	});
+}
+
+// update tally for ea vote
+export function vote(state, entry) {
+	return state.updateIn(
+		[ 'vote', 'tally', entry ], // reach into this nested data structure path
+		0,
+		tally => tally + 1
+	);
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,10 +1,10 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
-import { setEntries, next } from '../src/core';
+import { setEntries, next, vote } from '../src/core';
 
 describe('application logic', () => {
 
-	describe('setEntries', () => {
+	describe('setEntries', () => { // load entries
 
 		it('adds the entries to the state', () => {
 			const state = Map();
@@ -25,7 +25,7 @@ describe('application logic', () => {
 		});
 	});
 
-	describe('next', () => {
+	describe('next', () => { // start the voting
 
 		it('takes the next two entries under vote', () => {
 			const state = Map({
@@ -41,4 +41,58 @@ describe('application logic', () => {
 		});
 	});
 
+	describe('vote', () => { // add tally to entry for each vote
+
+		it('creates a tally for the voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 1
+					})
+				}),
+				entries: List()
+			}));
+		});
+
+		it('adds to existing tally for the voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 3,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
+			}));
+		});
+
+
+	});
+
 });
+
+
+
+
+
+
