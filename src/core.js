ef4f845a8e7242b9b835b279b9e1c24e5129a4c0
Jenna Vuong <jennavuong@gmail.com>
2016-04-25 22:43:13 -0700

    add next function to start the voting

--- src/core.js
+++ src/core.js
@@ -6,3 +6,12 @@ import { List } from 'immutable';
 export function setEntries(state, entries) {
 	return state.set('entries', List(entries)); // convert to immutable list
 }
+
+// start the voting by calling state that already has entries set
+export function next(state) {
+	const entries = state.get('entries');
+	return state.merge({
+		vote: Map({ pair: entries.take(2) }),
+		entries: entries.skip(2)
+	});
+}
