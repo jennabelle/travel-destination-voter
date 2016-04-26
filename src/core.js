def5aaaa90ade66a1cb533cc6e13e4812d4d4774
Jenna Vuong <jennavuong@gmail.com>
2016-04-25 22:04:02 -0700

    add support for loading of entries users will vote on

--- src/core.js
+++ src/core.js
@@ -1,3 +1,8 @@
+/* application allows 'loading in' a collection of entries that will be voted on */
+
+import { List } from 'immutable';
+
+// take previous state and collection of entries & produces state where entries are included
 export function setEntries(state, entries) {
-	return state.set('entries', entries);
+	return state.set('entries', List(entries)); // convert to immutable list
 }
