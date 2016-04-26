08d633927a38b12874665f7a45dc308f225f3bb3
Jenna Vuong <jennavuong@gmail.com>
2016-04-26 00:15:18 -0700

    add reducer and reducer spec

--- src/core.js
+++ src/core.js
@@ -11,10 +11,18 @@ export function setEntries(state, entries) {
 // create a vote Map({ pair: list })
 export function next(state) {
 	const entries = state.get('entries').concat(getWinners(state.get('vote')));
-	return state.merge({
-		vote: Map({ pair: entries.take(2) }),
-		entries: entries.skip(2)
-	});
+
+	if (entries.size === 1) {
+		return state.remove('vote')
+					.remove('entries')
+					.set('winner', entries.first());
+	}
+	else {
+		return state.merge({
+			vote: Map({ pair: entries.take(2) }),
+			entries: entries.skip(2)
+		});
+	}
 }
 
 // update tally for ea vote
---
+++ src/reducer.js
@@ -0,0 +1,5 @@
+export default function reducer(state, action) {
+	// Figure out which function to call and call it
+
+	
+}
