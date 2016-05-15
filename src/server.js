26690c161ab4272fb8c7bc68073deac8d34219dd
Jenna Vuong <jennavuong@gmail.com>
2016-05-15 00:48:24 -0700

    have store subscribe to state change events

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import Server from 'socket.io';
 
 // create a Socket.io server
-export default function startServer() {
+export default function startServer(store) {
 	const io = new Server().attach(8090); // bound to port 8090
+
+	// reads current state, turns it into plain JS object, emits it as state event on Socket.io server
+	store.subscribe(
+		() => io.emit('state', store.getState().toJS())
+	);
 }
